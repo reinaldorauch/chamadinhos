45bbd798edeb28cd67f7d1bd80db2fc73f36eeb6
Reinaldo A. C. Rauch <reinaldorauch@gmail.com>
2024-09-22 18:30:34 -0300

    Adding TicketCategory model

--- app/Models/TicketCategory.php
+++ app/Models/TicketCategory.php
@@ -5,11 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class TicketStatus extends Model
+class TicketCategory extends Model
 {
     use HasFactory;
 
-    protected $table = 'ticket_status';
+    protected $table = 'ticket_categories';
 
     public $timestamps = false;
 }
